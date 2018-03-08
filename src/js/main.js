2907d11d141bed51af47d51d565392b9802dc364
Florence Bolsée <florence.bolsee@gmail.com>
2018-03-08 11:48:17 +0100

    html page home

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,47 @@
-const name = "florence";
+var swiper = new Swiper('.swiper1', {
+      slidesPerView: 1,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+});
+
+var instagram = {
+  token: "1975026352.c6e2de8.3ef7709ab1e847beb0b6805c7c985fad",
+  count: '&count=20',
+  template: Handlebars.compile($("#insta-template").html()),
+  clientX: undefined,
+  overlayOn: false,
+  request: function(){
+    $.getJSON('https://api.instagram.com/v1/users/self/media/recent/?access_token=' + this.token + this.count)
+        .done( function(data) {
+          var insta = this.template(data);
+          $('.swiperInstagram .swiper-wrapper').append(insta);
+          var swiperInsta = new Swiper('.swiperInstagram', {
+                slidesPerView: 5,
+                slidesPerGroup: 4,
+                spaceBetween: 40,
+                loop: true,
+                pagination: {
+                  el: '.swiper-pagination',
+                  clickable: true,
+                },
+                navigation: {
+                  nextEl: '.swiper-button-next',
+                  prevEl: '.swiper-button-prev',
+                },
+              });
+              console.log(swiperInsta);
+        }.bind(this))
+        .fail (function() {
+          $('.block__instagram').css('display', 'none');
+    });
+  }
+};
+
+instagram.request();
